5e8a496de35c1c10fdebd85657cd775c7dc58245
Ariouati <ariouat_s@yahoo.com>
2017-01-30 09:10:01 +1100

    added some tools but new strategy not working yet

--- TME1.py
+++ TME1.py
@@ -12,7 +12,7 @@ from soccersimulator import settings
 import math
 import toolbox
         
-class MyStrategy(Strategy):
+class Attack(Strategy):
     def __init__(self):
         Strategy.__init__(self,"Ma strat")
     def compute_strategy(self,state,idteam,idplayer):
@@ -21,7 +21,7 @@ class MyStrategy(Strategy):
             return mystate.shoot(mystate.adv_but)
         return mystate.aller(mystate.ball_position())   
         
-class MyStrategy1(Strategy):
+class Defense(Strategy):
     def __init__(self):
         Strategy.__init__(self,"Ma strat")
     def compute_strategy(self,state,idteam,idplayer):
@@ -31,11 +31,44 @@ class MyStrategy1(Strategy):
         elif mystate.my_position().distance(mystate.ball_position())<40:
             return mystate.aller(mystate.ball_position())
         return mystate.aller((mystate.ball_position()-mystate.my_but).normalize()*10+mystate.my_but)
+        
+class Att1(Strategy):
+    def __init__(self):
+        Strategy.__init__(self,"Ma strat")
+    def compute_strategy(self,state,idteam,idplayer):
+        mystate = toolbox.MyState(state,idteam,idplayer)
+        if mystate.can_shoot():
+            return mystate.shoot(mystate.adv_but)
+        elif mystate.ball_position().x<90:
+            return mystate.aller(mystate.ball_position())
+        return mystate.aller((mystate.ball_position()-mystate.my_but).normalize()*10+mystate.my_but)
 
-joueur1 = Player("joueur 1", MyStrategy())
-joueur2 = Player("joueur 2", MyStrategy1())
+class Shadow(Strategy):
+    def __init__(self):
+        Strategy.__init__(self,"Ma strat")
+    def compute_strategy(self,state,idteam,idplayer):
+        mystate = toolbox.MyState(state,idteam,idplayer)
+        if mystate.can_shoot():
+            return mystate.shoot(mystate.adv_but)
+        return mystate.aller(mystate.opponentPosition()) 
+        
+class Intercept(Strategy):
+    def __init__(self):
+        Strategy.__init__(self,"Ma strat")
+    def compute_strategy(self,state,idteam,idplayer):
+        mystate = toolbox.MyState(state,idteam,idplayer)
+        if mystate.can_shoot():
+            return mystate.shoot(mystate.adv_but)
+        if (not mystate.closest()[0]):
+            if mystate.closest()[1]==0:
+                return mystate.aller(mystate.opponentposition()-mystate.ball_position()) 
+            return mystate.aller(mystate.theOther()-mystate.ball_position()) 
+        return mystate.aller(mystate.ball_position())   
+
+joueur1 = Player("john",Attack())
+joueur2 = Player("doe",Defense())
 print joueur1.name, joueur2.strategy, joueur2.name, joueur2.strategy
-team1 = SoccerTeam("Equipe 1", [joueur1, joueur2])
+team1 = SoccerTeam("barsa", [joueur1, joueur2])
 # nombre de joueurs de l equipe
 print(team1.nb_players)
 # renvoie la liste des noms, la liste des strategies
@@ -43,10 +76,10 @@ print(team1.players_name, team1.strategies)
 # nom et strategie du premier joueur
 print team1.player_name(0), team1.strategy(0)
 
-joueur3 = Player("joueur 3", MyStrategy())
-joueur4 = Player("joueur 4", MyStrategy1())
+joueur3 = Player("joueur 3",Intercept())
+joueur4 = Player("joueur 4", Defense())
 
-team2 = SoccerTeam("Equipe 2", [joueur3, joueur4])
+team2 = SoccerTeam("marseille", [joueur3, joueur4])
 # nombre de joueurs de l equipe
 print(team1.nb_players)
 # renvoie la liste des noms, la liste des strategies
--- toolbox.py
+++ toolbox.py
@@ -17,16 +17,42 @@ class MyState(object):
         self.key = (idteam,idplayer)
         self.my_but = Vector2D(settings.GAME_WIDTH/2+(-1)**(self.key[0])*settings.GAME_WIDTH/2,settings.GAME_HEIGHT/2) 
         self.adv_but = Vector2D(settings.GAME_WIDTH/2+(-1)**(self.key[0]+1)*settings.GAME_WIDTH/2,settings.GAME_HEIGHT/2)
+    
     def my_position(self):
         return self.state.player_state(self.key[0],self.key[1]).position
         #equivalent a self.player_state(self.key[0],self.key[1])
+    
+    def opponentPosition(self):
+        return (self.state.player_state(((self.key[0]+1)%2),0).position)
+        
+    def theOther(self):
+        return (self.state.player_state(((self.key[0]+1)%2),1).position)
+        
     def ball_position(self):
         return self.state.ball.position
+    
     def aller(self,p):
         return SoccerAction(p-self.my_position(),Vector2D())
+    
     def shoot(self,p):
         return SoccerAction(Vector2D(),p-self.my_position())
+    
     def can_shoot(self):
         return self.my_position().distance(self.ball_position())<=(settings.PLAYER_RADIUS+settings.BALL_RADIUS)
-
+    
+    def distanceToBall(self,a):
+        return (a-self.ball_position()).norm
+        
+    def closest(self):
+        goodSide=True
+        maxid=self.key[1]
+        maxi=self.distanceToBall(self.my_position())
+        if (self.distanceToBall(self.opponentPosition())>maxi):
+            maxid=0
+            goodSide=False
+        if (self.distanceToBall(self.theOther())>maxi):
+            maxid=1
+            goodSide=False
+        return (goodSide,maxid)
+        
 #test3

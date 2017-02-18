474d91be06139a8e6c42836ea90933fcb3ef3c07
LI <3305496@tarentule2.ufr-info-p6.jussieu.fr>
2017-02-18 22:41:18 +0100

    Nouvelles strategies

--- toolbox.py
+++ toolbox.py
@@ -18,9 +18,15 @@ class MyState(object):
         self.nbjoueurs=state.nb_players(1)
         self.my_but = Vector2D(settings.GAME_WIDTH/2+(-1)**(self.key[0])*settings.GAME_WIDTH/2,settings.GAME_HEIGHT/2) 
         self.adv_but = Vector2D(settings.GAME_WIDTH/2+(-1)**(self.key[0]+1)*settings.GAME_WIDTH/2,settings.GAME_HEIGHT/2)
-    
+        self.posA = Vector2D(settings.GAME_WIDTH/2+(-1)**(self.key[0]+1)**settings.GAME_WIDTH/2,settings.GAME_HEIGHT/2)
+        self.coeff_Def=(-1)**(self.key[0]) # -1 si Equipe 1 sinon 1
+        self.coeff_Att=(-1)**(self.key[0]+1) # 1 si Equipe 1 sinon -1
+
     def my_position(self):
         return (self.state.player_state(self.key[0],self.key[1]).position)
+        
+    def my_vitesse(self):
+        return (self.state.player_state(self.key[0],self.key[1]).vitesse).norm
         
     def allierPos(self,i):
         return (self.state.player_state((self.key[0]),i).position)
@@ -31,11 +37,11 @@ class MyState(object):
     def ball_position(self):
         return self.state.ball.position
     
-    def aller(self,p):
-        return SoccerAction(p-self.my_position(),Vector2D())
+    def aller(self,p,k=1):
+        return SoccerAction(k*(p-self.my_position()),Vector2D())
     
-    def shoot(self,p):
-        return SoccerAction(Vector2D(),p-self.my_position())
+    def shoot(self,p,k=1):
+        return SoccerAction(Vector2D(),k*(p-self.my_position()))
     
     def can_shoot(self):
         return self.my_position().distance(self.ball_position())<=(settings.PLAYER_RADIUS+settings.BALL_RADIUS)
@@ -60,4 +66,3 @@ class MyState(object):
 
     def ballPredict(self,t):
         return self.ball_position()+(self.state.ball.vitesse)*t
-
